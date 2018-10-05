790b5808f7b7e6185a79c03b464f88016f63ad8d
Popelkin <shurik.wps@gmail.com>
2018-10-05 23:02:37 +0300

    Init blog

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App;
+
+class TaskController extends Controller {
+
+    public function index() {
+        $tasks = App\Task::all();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function show($id) {
+        $task = App\Task::find($id);
+
+        return view('tasks.show', compact('task'));
+    }
+
+}
